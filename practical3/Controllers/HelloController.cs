36ad92c010057dda030775eee4dc569938650545
Bhanwar <bhanwarprajapati194@gmail.com>
2023-03-10 08:43:22 +0530

    Initial commit

---
+++ practical3/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace practical3.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
